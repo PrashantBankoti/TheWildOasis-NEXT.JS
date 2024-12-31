3a199ed806a2837999f24ca6d4b92450ef6cdc32
Prashant Bankoti <prashantbankoti@gmail.com>
2024-12-31 16:21:37 +0530

    added page and optimised image tag of next js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import logo from "@/public/logo.png";
+import Link from "next/link";
 function Logo() {
   return (
-    <a href="/" className="flex items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-4 z-10">
       {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
       <Image
         src={logo}
@@ -15,7 +16,7 @@ function Logo() {
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
-    </a>
+    </Link>
   );
 }
 
